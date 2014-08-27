d3f185ecde963bd2f538299711ba103f37847bdd
SirTobi <code.databyte@gmail.com>
2014-08-27 23:31:55 +0200

    tag repaired

--- include/logx/tag.hpp
+++ include/logx/tag.hpp
@@ -11,23 +11,23 @@ namespace logx {
 
 
 
-	class tag_base
+	class tag
 	{
 	public:
-		virtual ~tag_base() {}
+		virtual ~tag() {}
 		virtual std::wstring name() const = 0;
 		virtual std::wstring value() const = 0;
 	};
 
 
 	template<typename... Args>
-	class parameterized_tag_base : public tag_base
+	class parameterized_tag_base : public tag
 	{
 	protected:
 		typedef std::tuple<const Args...> tuple_type;
 	public:
-		parameterized_tag_base(Args&&... _args)
-			: mArgs(std::forward<Args>(_args)...)
+		parameterized_tag_base(Args... _args)
+			: mArgs(std::move(_args)...)
 		{}
 
 		virtual std::wstring value() const override
@@ -44,26 +44,20 @@ namespace logx {
 
 
 	template<typename C>
-	std::basic_ostream<C>& operator <<(std::basic_ostream<C>& _stream, const tag_base& _tag)
+	std::basic_ostream<C>& operator <<(std::basic_ostream<C>& _stream, const tag& _tag)
 	{
 		_stream << _tag.value();
 		return _stream;
 	}
 
-
-	template<typename NameTag>
-	struct tag;
-
 #define logxTAG(_name, _val_build, ...)																				\
-	template<>																										\
-	struct logx::tag<struct NameTag##_name> : public logx::parameterized_tag_base<__VA_ARGS__>						\
+	struct _name : public logx::parameterized_tag_base<__VA_ARGS__>						\
 	{																												\
-		template<typename... Args> tag(Args&&... _args) : parameterized_tag_base(std::forward<Args>(_args)...){}	\
-		virtual std::wstring name() const override { return LOGXW(#_name); }										\
-		virtual std::wstring _build_value(const tuple_type& _args) const override									\
-						{ return _val_build; }																		\
-	};	\
-	typedef logx::tag<struct NameTag##_name> _name;
+		template<typename... Args> _name(Args&&... _args) : logx::parameterized_tag_base<__VA_ARGS__>(std::forward<Args>(_args)...){}	\
+		inline virtual std::wstring name() const override { return LOGXW(#_name); }										\
+		inline virtual std::wstring _build_value(const tuple_type& _args) const override									\
+		{ return _val_build; }																		\
+	};
 }
 
 
