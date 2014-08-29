e4928cbcd2cbc4f545cf6d8a0cf205c8e2be21c1
SirTobi <code.databyte@gmail.com>
2014-08-29 08:15:56 +0200

    tags added (cat, log_level)

--- include/logx/stdtags.hpp
+++ include/logx/stdtags.hpp
@@ -28,6 +28,9 @@ namespace logx {
 
 		}
 
+
+		/**************************************************************************************************/
+		// tag: source
 		logxTAG(source,
 			(
 			details::trim_file_path(std::get<0>(_args)) + LOGXW(" in ")
@@ -38,12 +41,63 @@ namespace logx {
 			const wchar_t* /* file */,
 			const wchar_t* /* function */,
 			unsigned int /* line */
-			)
+		)
 #define logxSOURCE ::logx::tags::source(LOGXW(__FILE__), LOGXW(__FUNCTION__), __LINE__)
 
 
+		/**************************************************************************************************/
+		// tag: cat
+		logxTAG(cat,
+			(
+				std::get<0>(_args)
+			),
+			std::wstring
+		)
 
+		/**************************************************************************************************/
+		// tag: log_level
+		namespace details {
+			enum class _log_level : int8_t
+			{
+				TRACE = 0,
+				DEBUG,
+				INFO,
+				WARNING,
+				ERROR,
+				FATAL,
+				_LOGLVL_COUNT
+			};
 
+			static const wchar_t* LogLevelNames[int8_t(_log_level::_LOGLVL_COUNT)] =
+				{
+					LOGXW("TRACE"),
+					LOGXW("DEBUG"),
+					LOGXW("INFO"),
+					LOGXW("WARN"),
+					LOGXW("ERR"),
+					LOGXW("FATAL"),
+				};
+			
+			template <typename E>
+			auto to_underlying(E e) -> typename std::underlying_type<E>::type
+			{
+				return static_cast<typename std::underlying_type<E>::type>(e);
+			}
+		}
+
+		logxTAG(log_level,
+			(
+				details::LogLevelNames[details::to_underlying(std::get<0>(_args))]
+			),
+			details::_log_level
+		)
+
+		static log_level trace(details::_log_level::TRACE);
+		static log_level debug(details::_log_level::DEBUG);
+		static log_level info(details::_log_level::INFO);
+		static log_level warning(details::_log_level::WARNING);
+		static log_level error(details::_log_level::ERROR);
+		static log_level fatal(details::_log_level::FATAL);
 	}
 }
 
