477144ff36c096d64f3cf8ba4411b7ed45a25640
SirTobi <code.databyte@gmail.com>
2014-09-11 18:23:53 +0200

    added dump

---
+++ include/logx/dump.hpp
@@ -0,0 +1,122 @@
+#pragma once
+#ifndef _LOGX_DUMP_HPP
+#define _LOGX_DUMP_HPP
+
+#include <ostream>
+#include "config.hpp"
+
+namespace logx {
+
+	namespace details {
+
+
+		template<typename T, typename OutputTag>
+		class container_arg
+		{
+		public:
+			container_arg(container_arg&& _old)
+				: mData(std::move(_old.mData))
+			{
+			}
+
+			template<typename Iter>
+			container_arg(Iter _begin, Iter _end)
+				: mData(_begin, _end)
+			{
+			}
+
+			typename std::vector<T>::const_iterator begin() const
+			{
+				return mData.begin();
+			}
+
+			typename std::vector<T>::const_iterator end() const
+			{
+				return mData.end();
+			}
+		private:
+			std::vector<T> mData;
+		};
+
+		struct map_iterator_tag;
+		struct sequence_iterator_tag;
+		struct set_iterator_tag;
+	}
+
+	template<typename Iter>
+	details::container_arg<typename std::iterator_traits<Iter>::value_type, details::map_iterator_tag> mapped(Iter _begin, Iter _end)
+	{
+		return details::container_arg<typename std::iterator_traits<Iter>::value_type, details::map_iterator_tag>(_begin, _end);
+	}
+
+	template<typename Iter>
+	details::container_arg<typename std::iterator_traits<Iter>::value_type, details::sequence_iterator_tag> sequenced(Iter _begin, Iter _end)
+	{
+		return details::container_arg<typename std::iterator_traits<Iter>::value_type, details::sequence_iterator_tag>(_begin, _end);
+	}
+}
+
+namespace std {
+	template<typename Ch, typename It>
+	std::basic_ostream<Ch>& operator << (std::basic_ostream<Ch>& _stream, logx::details::container_arg<It, logx::details::sequence_iterator_tag> _it)
+	{
+		auto& begin = _it.begin();
+		auto& end = _it.end();
+		_stream << Ch('[');
+		if (begin != end)
+		{
+			_stream << *begin;
+
+			while (++begin != end)
+			{
+				_stream << Ch(',') << Ch(' ');
+				_stream << *begin;
+			}
+		}
+		_stream << Ch(']');
+		return _stream;
+	}
+
+	template<typename Ch, typename It>
+	std::basic_ostream<Ch>& operator << (std::basic_ostream<Ch>& _stream, logx::details::container_arg<It, logx::details::map_iterator_tag> _it)
+	{
+		auto& begin = _it.begin();
+		auto& end = _it.end();
+		_stream << Ch('[');
+		if (begin != end)
+		{
+			_stream << begin->first << Ch(' ') << Ch('=') << Ch('>') << Ch(' ') << begin->second;
+
+			while (++begin != end)
+			{
+				_stream << begin->first << Ch(' ') << Ch('=') << Ch('>') << Ch(' ') << begin->second;
+				_stream << *begin;
+			}
+		}
+		_stream << Ch(']');
+		return _stream;
+	}
+
+	template<typename Ch, typename It>
+	std::basic_ostream<Ch>& operator << (std::basic_ostream<Ch>& _stream, logx::details::container_arg<It, logx::details::set_iterator_tag> _it)
+	{
+		auto& begin = _it.begin();
+		auto& end = _it.end();
+		_stream << Ch('[');
+		if (begin != end)
+		{
+			_stream << *begin << Ch(' ') << Ch('=') << Ch('>') << Ch(' ');
+
+			while (++begin != end)
+			{
+				_stream << *begin << Ch(' ') << Ch('=') << Ch('>') << Ch(' ');
+				_stream << *begin;
+			}
+		}
+		_stream << Ch(']');
+		return _stream;
+	}
+}
+
+
+#endif
--- samples/simple/simple.cpp
+++ samples/simple/simple.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <map>
+#include <set>
 #include <logx.hpp>
 #include <logx/sink.hpp>
-
+#include <logx/dump.hpp>
 
 int main()
 {
-
+	std::set<float> xx = { 4.0f, 9.3f, 8.4f};
+	std::map<std::string, int> ages;
+	ages["tobi"] = 22;
+	ages["tally"] = 23;
+	ages["jan"] = 34;
 	std::vector<int> vec = { 1, 2, 3 };
-	logx::log("here comes a string: $1", logxSOURCE);
+	logx::log("here comes a string: $1", logx::mapped(ages.begin(), ages.end()),logxSOURCE);
 
 	logx::log("here comes a string: $1, $2, $3", 1.0f, "test", L"wie gehts");
 
